d054d4418d8f5bdf919c22c4a5d3b146993fd93b
softprops <d.tangren@gmail.com>
2017-08-03 11:30:28 -0400

    resolve email address from mentions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 // https://github.com/slackapi/node-slack-sdk#posting-a-message-with-web-api
 var WebClient = require('@slack/client').WebClient
 var slack = new WebClient(process.env.SLACK_BOT_TOKEN);
+const mentiondb = require('./mentiondb.json')
 
 // google api
 const google = require('googleapis');
@@ -13,8 +14,6 @@ const calendar = google.calendar('v3')
 const key = require('./credentials.json')
 
 // fixme: may need to respond quickly or slack ui will error out
-
-// 1 create a map @mention -> emails
 
 // 2 parse @mention names from cmd.text ( may need bot api credential )
 // https://github.com/slackapi/node-slack-sdk add npm slack client
@@ -30,8 +29,18 @@ const key = require('./credentials.json')
 // successful message payload or string error
 const command = (cmd) => {
   return new Promise((resolve, reject) => {
+    const mentions = cmd.text.trim().split(/\s+/)
+    const emails = mentions.reduce(
+      (acc, mention) => {
+        const email = mentiondb[mention]
+        if (email) {
+          acc.push(email)
+          return acc
+        }
+      }, []
+    )
     resolve({
-      text: `Well hello there ${cmd.user_name}. Looks like you figured this much out. Go <https://api.slack.com/slash-commands|here> next`
+      text: `test ${emails}`
     })
   })
 }
